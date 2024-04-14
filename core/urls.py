f9c97ba456427bd697c55eadfbafa46e4240f29b
Bruno Garcia <brunogalberti@gmail.com>
2024-04-14 17:31:53 -0300

    Auth Update 0.2

--- core/urls.py
+++ core/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     path('api/', include('Course_app.urls')),
     path('', include('authentification.urls')),
     
-    path('token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
-    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
+    path('/token/', jwt_views.TokenObtainPairView.as_view(), name ='token_obtain_pair'),
+    path('/token/refresh/', jwt_views.TokenRefreshView.as_view(), name ='token_refresh'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
